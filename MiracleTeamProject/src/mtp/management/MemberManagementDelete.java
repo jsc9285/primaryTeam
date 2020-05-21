e1ff41c7448c216e97a5c4d9c8c47ff4705b82fd
TJ <TJ@DESKTOP-J2LI003>
2020-05-21 16:13:22 +0900

    화면관리 (삭제 수정) 20200521

--- MiracleTeamProject/src/mtp/management/MemberManagementDelete.java
+++ MiracleTeamProject/src/mtp/management/MemberManagementDelete.java
@@ -19,26 +19,37 @@ public class MemberManagementDelete extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		Connection conn = null;
 
-		int no = Integer.parseInt(req.getParameter("no"));
+		String mNo = "";
+		String aNo = "";
+		
 
 		try {
+			mNo = req.getParameter("mmNo");
+			aNo = req.getParameter("adNo");
+			
 			ServletContext sc = this.getServletContext();
 
 			conn = (Connection) sc.getAttribute("conn");
 
 			MemberDao memberDao = new MemberDao();
 			memberDao.setConnection(conn);
-
-			int admin = memberDao.adminDataDelete(no);
-			int result = memberDao.MemberDataDelete(no);
-
-			if (admin == 0) {
-				System.out.println("관리자 삭제를 실패하였습니다.");
+			
+			if (mNo != null) {
+				int no = Integer.parseInt(mNo);
+				int result = memberDao.MemberDataDelete(no);
+				if (result == 0) {
+					System.out.println("회원 삭제를 실패하였습니다.");
+				}
+			} else if (aNo != null) {
+				int abNo = Integer.parseInt(aNo);
+				int admin = memberDao.adminDataDelete(abNo);
+				if (admin == 0) {
+					System.out.println("관리자 삭제를 실패하였습니다.");
+				}
 			}
 
-			if (result == 0) {
-				System.out.println("회원 삭제를 실패하였습니다.");
-			}
+
+
 
 			res.sendRedirect("./list");
 		} catch (Exception e) {
